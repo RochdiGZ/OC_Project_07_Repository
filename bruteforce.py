ffeb628ace88eb5811cbb2f3ffe839ed57be0105
Rochdi GUEZGUEZ <rochdi.guezguez@gmail.com>
2022-12-23 01:13:45 +0100

    Add 3th commit

--- bruteforce.py
+++ bruteforce.py
@@ -1,76 +1,33 @@
-import csv
 import time
 from itertools import combinations
-
-PRICE_MAX = 500
-
-
-def get_data_csv(file_name: str) -> list:
-    with open(f"dataset/{file_name}.csv", "r", encoding="utf-8") as f:
-        data_csv = csv.reader(f)
-        data = []
-        for line in data_csv:
-            data.append(line)
-    return data
+from algorithm import Algorithm
 
 
-def get_actions(data: list) -> list:
-    actions = []
-    for i in range(1, len(data)):
-        actions.append((data[i][0], float(data[i][1]), float(data[i][2])))
-    return actions
-
-
-def actions_price(actions: list) -> float:
-    sum_prices = sum([float(price) for _, price, _ in actions])
-    return round(sum_prices, 3)
-
-
-def actions_profit(actions: list) -> float:
-    sum_profits = sum([float(price) * float(profit) / 100 for _, price, profit in actions])
-    return round(sum_profits, 3)
-
-
-def display_result(file_name, best_actions: list):
-    print(f"The chosen file is {file_name}.csv which contains the all actions data")
-
-    # sort by price the data of best actions to buy
-    best_actions = sorted(best_actions, key=lambda d: d[1], reverse=True)
-    print("\nThe", len(best_actions), "best actions to buy :")
-    for action in best_actions:
-        print(action[0])
-
-    print("\nTotal cost :", actions_price(best_actions), "€")
-
-    print("Total profit :", actions_profit(best_actions), "€")
-
-
-def brute_force(file_name: str) -> list:
-    actions = get_actions(get_data_csv(file_name))
+def brute_force(actions: list, max_cost: int = 5000) -> list:
     best_combination = []
-    profit_max = 0
+    max_profit = 0
     for i in range(1, len(actions) + 1):
         for combination in combinations(actions, i):
-            # get a sum of prices for each combination
-            price = actions_price(list(combination))
-            if price <= PRICE_MAX:
-                # get a sum of profits for each combination
-                profit = actions_profit(list(combination))
-                if profit > profit_max:
-                    profit_max = profit
-                    best_combination = list(combination)
+            # get a total cost for each combination
+            cost = Algorithm.total_cost(list(combination))
+            if cost <= max_cost:
+                # get a total profit for each combination
+                profit = Algorithm.total_profit(list(combination))
+                if profit > max_profit:
+                    max_profit = profit
+                    best_combination = combination
     return best_combination
 
 
 def brute_force_result():
-    csv_file = "dataset0"
+    print("\nRunning of Brute force algorithm with dataset0")
+    algo = Algorithm.bruteforce()
+    actions = algo.actions
     start = time.time()
-    result = brute_force(csv_file)
-    # Display the data of best actions to buy with 500 € for the best combination
-    display_result(csv_file, result)
+    selected_actions = brute_force(actions)
+    # Display the data of selected actions to buy
+    algo.display_names(selected_actions)
+    algo.display_total_cost(selected_actions)
+    algo.display_total_profit(selected_actions)
     end = time.time()
-    print("\nDuration of program running :", round(end-start, 4), "seconds.")
-
-
-if __name__ == "__main__":
-    brute_force_result()
+    print("Duration of program running :", round(end - start, 2), "seconds.")
--- optimized.py
+++ optimized.py
@@ -1,80 +1,87 @@
-import csv
 import time
-
-PRICE_MAX = 500
-
-
-def get_data_csv(file_name: str) -> list:
-    with open(f"dataset/{file_name}.csv", "r", encoding="utf-8") as f:
-        data_csv = csv.reader(f)
-        data = []
-        for line in data_csv:
-            data.append(line)
-    return data
+from algorithm import Algorithm
 
 
-def get_actions(data: list) -> list:
-    actions = []
-    for i in range(1, len(data)):
-        if float(data[i][1]) > 0 and float(data[i][2]) > 0:
-            actions.append((data[i][0], float(data[i][1]), float(data[i][2])))
-    return actions
+def knapsack(actions: list, max_cost: int = 500000) -> list:
+    n = len(actions)
+    m = [[float(0) for _ in range(max_cost + 1)] for _ in range(n + 1)]
+
+    # Build matrix which contains max profits
+    for i in range(n + 1):
+        for c in range(max_cost + 1):
+            cost = actions[i - 1].price
+            profit = actions[i - 1].profit
+            if i == 0 or c == 0:
+                m[i][c] = 0
+            elif cost <= c:
+                m[i][c] = max(profit + m[i - 1][c - cost], m[i - 1][c])
+            else:
+                m[i][c] = m[i - 1][c]
+
+    # Find the best combinations
+    c = max_cost
+    r = len(actions)
+    selected_actions = []
+    while c >= 0 and r >= 0:
+        action = actions[r - 1]
+        if m[r][c] == m[r - 1][c - action.price] + action.profit:
+            selected_actions.append(action)
+            c -= action.price
+        r -= 1
+
+    return selected_actions   # m[n][max_cost] = m[-1][-1]
 
 
-def actions_price(actions: list) -> float:
-    sum_prices = sum([float(price) for _, price, _ in actions])
-    return round(sum_prices, 3)
+def knapsack_result():
+    for i in range(3):
+        if i == 0:
+            print("\nRunning of knapsack algorithm with dataset" + str(i))
+        elif i == 1:
+            print("\nRunning of knapsack algorithm with dataset" + str(i))
+        elif i == 2:
+            print("\nRunning of knapsack algorithm with dataset" + str(i))
+        algo = Algorithm.optimized(i)
+        actions = algo.actions
+        actions = algo.multiply_price(actions)
+        start = time.time()
+        selected_actions = knapsack(actions)
+        # Update each price of selected actions (divide each price per 100)
+        best_actions = algo.divide_price(selected_actions)
+        # Display the data of selected actions to buy
+        algo.display_names(best_actions)
+        algo.display_total_cost(best_actions)
+        algo.display_total_profit(best_actions)
+        end = time.time()
+        print("Duration of program running :", round(end - start, 2), "seconds.")
 
 
-def actions_profit(actions: list) -> float:
-    sum_profits = sum([float(price) * float(profit) / 100 for _, price, profit in actions])
-    return round(sum_profits, 3)
-
-
-def display_result(file_name, best_actions: list):
-    print(f"The chosen file is {file_name}.csv which contains the all actions data")
-
-    # sort by price the data of best actions to buy
-    best_actions = sorted(best_actions, key=lambda d: d[1], reverse=True)
-    print("\nThe", len(best_actions), "best actions to buy:")
-    for action in best_actions:
-        print(action[0])
-
-    print("\nTotal cost :", actions_price(best_actions), "€")
-
-    print("Total profit :", actions_profit(best_actions), "€")
-
-
-def best_invest(file_name: str) -> list:
-    actions = get_actions(get_data_csv(file_name))
-    # sort by profit pourcentage the all data
-    actions = sorted(actions, key=lambda d: d[2], reverse=True)
-    list_actions = []
-    price_max = PRICE_MAX
-    for action in actions:
-        if price_max >= action[1]:
-            list_actions.append(action)
-            price_max -= action[1]
-    return list_actions
-
-
+"""
 def choose_file() -> str:
     number = input("Please, enter 0 or 1 or 2 for choosing a csv file : ")
     if number in ["0", "1", "2"]:
-        return "dataset" + number
-    else:
-        return choose_file()
+        return number
+    return choose_file()
 
 
-def best_invest_result():
-    csv_file = choose_file()
-    start = time.time()
-    result = best_invest(csv_file)
-    # Display the data of best actions to buy with 500 €
-    display_result(csv_file, result)
-    end = time.time()
-    print("\nDuration of program running :", round(end-start, 4), "seconds.")
-
-
-if __name__ == "__main__":
-    best_invest_result()
+def knapsack_result0():
+    ok = True
+    while ok:
+        choice = input("Would do you like testing the program ? (Yes/No) : ")
+        if choice.lower() == "yes" or choice.lower() == "y":
+            i = int(choose_file())
+            algo = Algorithm.optimized(i)
+            actions = algo.actions
+            actions = multiply_price(actions)
+            start = time.time()
+            selected_actions = knapsack(actions)
+            # Update each price of selected actions (divide each price per 100)
+            best_actions = divide_price(selected_actions)
+            # Display the data of selected actions to buy
+            algo.display_names(best_actions)
+            algo.display_total_cost(best_actions)
+            algo.display_total_profit(best_actions)
+            end = time.time()
+            print("\nDuration of program running :", round(end - start, 2), "seconds.")
+        else:
+            ok = False
+"""
